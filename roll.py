dd035c30d82f99a6e36daa40243f393d7b9629c3
Quasar Jarosz <quasar@ja.rosz.org>
2014-06-24 09:45:22 -0500

    Improvements to roll code

--- roll.py
+++ roll.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 import random
 random.seed()
 
@@ -11,18 +13,18 @@ def count_hits(rolls):
             count += 1
     return count
 
-def count_ones(rolls):
+def count_misses(rolls):
     count = 0
     for i in rolls:
         if i == 1:
             count += 1
     return count
 
-def judge_rolls(rolls):
+def score_rolls(rolls):
     hits = count_hits(rolls)
-    misses = count_ones(rolls)
+    misses = count_misses(rolls)
 
-    if misses >= len(rolls) / 2:
+    if misses >= len(rolls) / 2.0:
         if hits == 0:
             return "CRITICAL GLITCH"
         else:
@@ -35,16 +37,24 @@ def roll(dice):
     for i in range(dice):
         rolls.append(roll_d6())
 
-    return (judge_rolls(rolls), rolls)
+    return (score_rolls(rolls), rolls)
 
 def test():
-    assert judge_rolls([1, 2, 2, 3]) == 0
-    assert judge_rolls([1, 1, 2, 5]) == "Glitch"
-    assert judge_rolls([1, 1, 2, 3]) == "CRITICAL GLITCH"
+    assert score_rolls([1, 2, 2, 3]) == 0
+    assert score_rolls([6, 6, 6, 6]) == 4
+    assert score_rolls([1, 1, 2, 5]) == "Glitch"
+    assert score_rolls([1, 1, 2, 3]) == "CRITICAL GLITCH"
+    assert score_rolls([1, 1, 2]) == "CRITICAL GLITCH"
+    assert score_rolls([1, 2, 2]) == 0
+    assert score_rolls([1, 1, 1, 2, 2]) == "CRITICAL GLITCH"
+    assert score_rolls([1, 1, 1, 2, 6]) == "Glitch"
+    assert score_rolls([1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 
+                        4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6]) == 7
 
 if __name__ == '__main__':
     print "Performing self test..."
     test()
+    print "All tests passed! Looks like your environment is sane.\n"
 
     while True:
         dice = raw_input("How many dice to roll? ")
